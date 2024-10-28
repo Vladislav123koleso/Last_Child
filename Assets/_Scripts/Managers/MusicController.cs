ed8a5a8612840f7efe8a2a015685a545bec0651a
Vladislav Kolesnichenko <vlad09koleso@gmail.com>
2024-10-28 18:31:58 +0300

    sound

--- Assets/_Scripts/Managers/MusicController.cs
+++ Assets/_Scripts/Managers/MusicController.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 
 public class MusicController : MonoBehaviour
 {
-    public AudioSource _audioSource;
+    public AudioSource _audioSource1;
+    public AudioSource _audioSource2;
     public Slider _volumeSlider;
 
     private void Start()
     {
         // Устанавливаем начальную громкость и значение слайдера
-        _volumeSlider.value = _audioSource.volume;
+        _volumeSlider.value = _audioSource1.volume;
+        _volumeSlider.value = _audioSource2.volume;
         _volumeSlider.onValueChanged.AddListener(ChangeVolume);
     }
 
     private void ChangeVolume(float volume)
     {
-        _audioSource.volume = volume;
+        _audioSource1.volume = volume;
+        _audioSource2.volume = volume;
     }
 }
